f4f3ba936c4dbf71e9a49d859a3aa3b605cf6031
Ruggero Visconti <er_mejo85@hotmail.it>
2012-01-21 11:55:55 +0000

    RisorseXML funzionante

---
+++ src/risorseXML/pdm/RisorseXMLActivity.java
@@ -0,0 +1,53 @@
+package risorseXML.pdm;
+
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.app.Activity;
+import android.content.res.XmlResourceParser;
+import android.os.Bundle;
+import android.util.Log;
+
+public class RisorseXMLActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        XmlResourceParser parser = getResources().getXml(R.xml.compilation);
+        try
+        {
+        	int eventType = parser.getEventType();
+        	while (eventType != XmlResourceParser.END_DOCUMENT)
+        	{
+        		if (eventType == XmlResourceParser.START_TAG)
+        		{
+        			String tagName = parser.getName();
+        			if ("brano".equals(tagName))
+        			{
+        				String id = parser.getAttributeValue(0);
+        				Log.d("XML PARSER",id);
+        			}
+        		}
+        			else if (eventType== XmlResourceParser.TEXT)
+        			{
+        				String elementValue = parser.getText();
+        				Log.d("XML PARSER", elementValue);
+        			}
+        			eventType = parser.next();
+        	}
+        }
+        		catch (XmlPullParserException e)
+        		{
+        			e.printStackTrace();
+        		}
+        		catch (IOException e)
+        		{
+        			e.printStackTrace();
+        		}
+    }
+}
